21f77a1b750ef07d175c33c7a0d018994ff64876
method <mamatisaevanurisa@icloud.com>
2023-07-17 02:05:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int san = scan.nextInt();
+//      if(san==100){
+//          throw new Error();
+//      }
+//        System.out.println((san+san));
+
+//        int san2 = scan.nextInt();
+//        try {
+//            System.out.println(san / san2);
+//        } catch (ArifmeticException arifmeticException) {//new
+//            System.out.println("Sandy nolgo bolgongo bolboit");
+//        }
+//        System.out.println("Hello Java");
+        // System.out.println(3+Integer.parseInt("5"));
+
+//        int[] arr = new int[10];
+//        try{
+//            System.out.println(arr[100]);
+//        }
+//        catch(ArrayIndexOutOfBoundsException e){
+//            //e.printStackTrace();
+//            System.out.println(e.getMessage());
+//        }
+//        System.out.println("Java");
+
+
+//        method();
+
+//    public static void method() throws IOException { //(int san)
+//        //FileWriter writer = new FileWriter("text.txt");
+//        if (san ==10){
+//            throw new Exception("tuura emes");
+//
+//        }
+//}
+
+
+
+
+    }
+}
